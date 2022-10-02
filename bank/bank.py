f9ff32cee33717e381b011843626f90bfbe95c5c
Cameron Arredondo <cam.arredondo@outlook.com>
2022-10-02 00:07:43 -0500

    remove requirements from tests and update code

--- bank/bank.py
+++ bank/bank.py
@@ -2,11 +2,15 @@ class Account:
     def __init__(self, name):
         self.name = name
         self.balance = 0
-    
-    def make_money(self, amount):
+
+    def deposit_money(self, amount):
         self.balance += amount
 
+    def make_withdrawl(self, amount):
+        self.balance -= amount
+        
 class Bank:
     def open_account(self, name):
         return Account(name)
+
 
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,22 +1,25 @@
 import pytest
 from bank.bank import Bank
 
-@pytest.fixture(
-    name="account"
-)
+
+@pytest.fixture(name="account")
 def make_account():
     bank = Bank()
-    
-    return bank.open_account( name = "Cameron")
+
+    return bank.open_account(name="Cameron")
 
 
 def test_open_account(account):
-     assert account.name == "Cameron"
+    assert account.name == "Cameron"
+
 
 def test_deposit_money(account):
-   
-   
-    account.make_money(
-        100
-    )
+    account.deposit_money(100)
     assert account.balance == 100
+
+def test_make_withdrawl(account):
+    account.balance = 100
+    account.make_withdrawl(
+        20
+    )
+    assert account.balance == 80
